42f333933f0c081a2afbc5afee7a2582c9fa8815
LM2124 <134977727+LM2124@users.noreply.github.com>
2025-03-13 09:41:01 -0300

    Brincando um pouco com os componentes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+
+const frases = [
+  "Recombobulando o Turbo-Encabulador...",
+  "Deletando system32...",
+  "Deletando node_modules...",
+  "Falando mal do Angular...",
+  "Inventando polêmica no Reddit...",
+  "Liderando uma guerra contra o Electron...",
+  "Considerando trocar pra backend...",
+  "Ajustando coeficiente de Squimble...",
+  "Desafiando Deus...",
+  "Pedindo ideias pro ChatGPT...",
+]
+
+function fraseAleatoria() {
+  return frases[Math.floor(Math.random() * frases.length)];
+}
+
+const Frase = () => {
+  return <Text>{fraseAleatoria()}</Text>;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ActivityIndicator size="large" color="blue" />
+      <Frase />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +37,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
